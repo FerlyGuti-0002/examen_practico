290b39e7089157f066d321c001253f5c9f7212df
Torrez Rolan <FerlyGuti-0002>
2021-04-07 17:05:25 -0600

    commit 2 examen

---
+++ examenpractico/app/Http/Controllers/ProfesoresController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfesoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('profesores.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $profesores = new Profesores();
+        $profesores->DNI = $request->get('DNI');
+        $profesores->Nombres = $request->get('nombres');
+        $profesores->Apellidos = $request->get('apellidos');
+        $profesores->Direccion = $request->get('direccion');
+        $profesores->Titulo = $request->get('titulo');
+        $profesores->Telefono = $request->get('telefono');
+       
+        $profesores->save();
+
+        return redirect('/profesores');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ examenpractico/database/migrations/2021_04_07_223710_create_profesores_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfesoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profesores', function (Blueprint $table) {
+            $table->id();
+            $table->DNI();
+            $table->string('nombres', 15);
+            $table->string('apellidos', 15);
+            $table->string('direccion', 40);
+            $table->string('titulo', 20);
+            $table->int('telefono', 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profesores');
+    }
+}
